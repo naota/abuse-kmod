edc87f88ff5d1a8e43f660faad743c73d8c97b32
Naohiro Aota <naota@elisp.net>
2015-06-10 18:31:59 +0900

    add remove test

--- userland/abuse-add.c
+++ userland/abuse-add.c
@@ -3,7 +3,9 @@
 #include <assert.h>
 #include <errno.h>
 #include <sys/ioctl.h>
+#include <sys/stat.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "abuse.h"
 
@@ -16,14 +18,44 @@ int main()
 
   // cannot add existing device
   int r = ioctl(fd, ABUSE_CTL_ADD, 0); err = errno;
-  perror(NULL);
+  perror("add existing");
   assert(r < 0);
   assert(err == EEXIST);
 
-  // add unused device
-  r = ioctl(fd, ABUSE_CTL_ADD, 10);
-  perror(NULL);
-  assert(r == 10);
+  const int NEWDEV = 10;
+  struct stat s;
+  char devname[32];
+  sprintf(devname, "/dev/abuse%d", NEWDEV);
+
+  r = stat(devname, &s); err = errno;
+  perror("missing device check");
+  assert(r == -1);
+  assert(err == ENOENT);
+
+  // add missing device
+  r = ioctl(fd, ABUSE_CTL_ADD, NEWDEV); err = errno;
+  perror("add missing");
+  assert(r == NEWDEV);
+
+  r = stat(devname, &s); err = errno;
+  perror("added device check");
+  assert(r == 0);
+
+  // remove alloced device
+  r = ioctl(fd, ABUSE_CTL_REMOVE, NEWDEV); err = errno;
+  perror("remove device");
+  assert(r == NEWDEV);
+
+  r = stat(devname, &s); err = errno;
+  perror("removed device check");
+  assert(r == -1);
+  assert(err == ENOENT);
+
+  // remove missing device
+  r = ioctl(fd, ABUSE_CTL_REMOVE, NEWDEV); err = errno;
+  perror("remove missing device");
+  assert(r == -1);
+  assert(err == ENOENT);
 
   close(fd);
   return 0;
